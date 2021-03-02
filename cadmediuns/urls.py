988df77aa291d58e6b19b55fac1aaceeab5c7721
Moraes <slsmoraes@gmail.com>
2021-03-02 19:12:52 -0300

    D20210301H1849-Crud Update no cadastro de  Mediuns

--- cadmediuns/urls.py
+++ cadmediuns/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from s2itdrbello.views import cadmediuns_lista, cadmediuns_insert
+from s2itdrbello.views import cadmediuns_lista
+from s2itdrbello.views import cadmediuns_insert
+from s2itdrbello.views import cadmediuns_update
 
 urlpatterns = [
     path('lista/', cadmediuns_lista, name="cadmediuns_lista"),
     path('insert/', cadmediuns_insert, name="cadmediuns_insert"),
+    path('update/<int:id>/', cadmediuns_update, name="cadmediuns_update"),
 ]
--- s2itdrbello/views.py
+++ s2itdrbello/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from cadmediuns.models import CadastroMediuns
 from cadmediuns.forms import CadastroMediunsForm
@@ -8,7 +8,15 @@ def cadmediuns_lista(request):
     return render(request, 'cadmediuns_lista.html', {'listamediuns': listamediuns})
 
 def cadmediuns_insert(request):
-    form = CadastroMediunsForm(request.POST, None)
+    form = CadastroMediunsForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('cadmediuns_lista')
+    return render(request, 'cadmediuns.html', {'form': form})
+
+def cadmediuns_update(request, id):
+    cadmediuns = get_object_or_404(CadastroMediuns, pk=id)
+    form = CadastroMediunsForm(request.POST or None, request.FILES or None, instance=cadmediuns)
     if form.is_valid():
         form.save()
         return redirect('cadmediuns_lista')
