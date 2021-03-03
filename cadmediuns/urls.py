6843f9cde5e627fcd60ad547bd43c9a8b962b99b
Moraes <slsmoraes@gmail.com>
2021-03-03 14:18:11 -0300

    D20210303H1417-Crud Delete no cadastro de  Mediuns

--- cadmediuns/urls.py
+++ cadmediuns/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from s2itdrbello.views import cadmediuns_lista
 from s2itdrbello.views import cadmediuns_insert
 from s2itdrbello.views import cadmediuns_update
+from s2itdrbello.views import cadmediuns_delete
+
+
 
 urlpatterns = [
     path('lista/', cadmediuns_lista, name="cadmediuns_lista"),
     path('insert/', cadmediuns_insert, name="cadmediuns_insert"),
     path('update/<int:id>/', cadmediuns_update, name="cadmediuns_update"),
+    path('delete/<int:id>/', cadmediuns_delete, name="cadmediuns_delete"),
 ]
--- s2itdrbello/views.py
+++ s2itdrbello/views.py
@@ -21,3 +21,11 @@ def cadmediuns_update(request, id):
         form.save()
         return redirect('cadmediuns_lista')
     return render(request, 'cadmediuns.html', {'form': form})
+
+def cadmediuns_delete(request, id):
+    cadmediuns = get_object_or_404(CadastroMediuns, pk=id)
+
+    if request.method == 'POST':
+        cadmediuns.delete()
+        return redirect('cadmediuns_lista')
+    return render(request, 'cadmediuns_delete_confirm.html', {'cadmediuns': cadmediuns})
